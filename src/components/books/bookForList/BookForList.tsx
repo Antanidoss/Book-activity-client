58b74db25ba8b55a11ec05f16366a8faff798a96
Antanidoss <antosci2002@gmail.com>
2022-10-11 20:02:44 +0300

    Added base url

--- src/components/books/bookForList/BookForList.tsx
+++ src/components/books/bookForList/BookForList.tsx
@@ -22,7 +22,7 @@ const Book: React.FC<BookType> = (props) => {
                     </Col>
                 </Link>
                 <Col span={24} style={{top: "40px"}}>
-                    <Statistic prefix="Users rated:" style={{float: "right", marginRight: "12px"}} valueStyle={{fontSize: "15px"}} value={props.bookRating.bookOpinions.length} />
+                    <Statistic prefix="Users rated:" style={{float: "right", marginRight: "12px"}} valueStyle={{fontSize: "11px"}} value={props.bookRating.bookOpinions.length} />
                 </Col>
                 <Row style={{marginTop: "55px"}}>
                     <Col span={12} style={{ left: "10px"}}>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Avatar, Menu, Image } from "antd";
 import {
     UserOutlined,
@@ -11,27 +11,28 @@ import {
 import { PropsType } from '../navbar/NavbarContainer';
 
 const Navbar: React.FC<PropsType> = (props) => {
+    const location = useLocation();
     return (
-        <Menu theme="dark" mode="inline" style={{ alignContent: 'center' }}>
+        <Menu theme="dark" mode="inline" style={{ alignContent: 'center' }} selectedKeys={[location.pathname]}>
             {
                 props.isAuthenticated
-                    ? <Menu.SubMenu key={1} style={{ marginTop: "64px" }} title={props.userName}
+                    ? <Menu.SubMenu key="/userOptions" style={{ marginTop: "64px" }} title={props.userName}
                         icon={props.avatarImage?.byteLength ? <Avatar><Image width={"32px"} src={("data:image/png;base64," + props.avatarImage)} /></Avatar> : <UserOutlined />}>
                         <Menu.Item>Profile</Menu.Item>
-                        <Menu.Item><Link to="/administration">Administration</Link></Menu.Item>
+                        <Menu.Item key="/administration" ><Link to="/administration">Administration</Link></Menu.Item>
                     </Menu.SubMenu>
                     : <Menu.Item key={1} icon={React.createElement(LoginOutlined)} style={{ marginTop: "64px" }}><Link to="/login">LOGIN</Link></Menu.Item>
             }
-            <Menu.Item key={2} icon={React.createElement(BookOutlined)}>
+            <Menu.Item key="/activeBooks" icon={React.createElement(BookOutlined)}>
                 <Link to="/activeBooks">ACTIVE BOOKS</Link>
             </Menu.Item>
-            <Menu.Item key={3} icon={React.createElement(BookOutlined)}>
+            <Menu.Item key="/books" icon={React.createElement(BookOutlined)}>
                 <Link to={"/books"}>BOOKS</Link>
             </Menu.Item>
-            <Menu.Item key={4} icon={React.createElement(BarChartOutlined)}>
+            <Menu.Item key="/statistics" icon={React.createElement(BarChartOutlined)}>
                 <Link to="#">STATISTICS</Link>
             </Menu.Item>
-            <Menu.Item key={5} icon={React.createElement(TeamOutlined)}>
+            <Menu.Item key="/friends" icon={React.createElement(TeamOutlined)}>
                 <Link to={"#"}>FRIENDS</Link>
             </Menu.Item>
         </Menu>
