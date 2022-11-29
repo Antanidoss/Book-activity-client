a1c4309b63ca054915b25d8bd557f8d09805fa34
Antanidoss <antosci2002@gmail.com>
2022-11-29 19:48:10 +0300

    Added statistics display

--- src/components/activeBooksStatistic/ActiveBooksStatistic.tsx
+++ src/components/activeBooksStatistic/ActiveBooksStatistic.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { ActiveBooksStatisticType } from "../../types/activeBooksStatisticType";
+import { Divider, List } from 'antd';
 
 const ActiveBooksStatistic: React.FC<ActiveBooksStatisticType> = (statistic: ActiveBooksStatisticType) => {
     return (
-        <div>{statistic.averagePagesReadPerDay}</div>
+        <>
+            <Divider orientation="center">Total stats</Divider>
+            <div style={{"textAlign": "center", "marginTop": "50px"}}>
+                <h3>Average number of pages read per day: {statistic.averagePagesReadPerDay}</h3>
+                <h3>Average number of pages read per week: {statistic.averagePagesReadPerWeek}</h3>
+                <h3>Average number of pages read per mouth: {statistic.averagePagesReadPerMouth}</h3>
+            </div>
+        </>
     )
 }
 
