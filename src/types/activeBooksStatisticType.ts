596fcf5e0a6c83b04e976891238d8b86fea98729
Antanidoss <antosci2002@gmail.com>
2023-07-08 12:29:40 +0300

    Statistics calendar styling

--- src/types/activeBooksStatisticType.ts
+++ src/types/activeBooksStatisticType.ts
@@ -1,7 +1,12 @@
 export type ActiveBooksStatisticType = {
-    numberOfPagesReadPerDay: Array<number>,
+    readingCalendar: Array<NumberOfPagesReadPerDay>,
     averagePagesReadPerDay: number,
     averagePagesReadPerWeek: number,
     averagePagesReadPerMouth: number,
     amountDaysOfReads: number
+}
+
+export type NumberOfPagesReadPerDay = {
+    countPagesRead: number,
+    date: string
 }
