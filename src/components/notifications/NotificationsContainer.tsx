afb93c3ee7c4bd9873a65ea546a83c057443db27
Antanidsoss <antosci2002@gmail.com>
2023-12-11 20:44:26 +0300

    notifications improvements

--- src/components/notifications/NotificationsContainer.tsx
+++ src/components/notifications/NotificationsContainer.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { AppStoreType, ExtractConnectType } from "../../redux/redux-store";
-import { addNotification, getUserNotificationsThunkCreator } from "../../redux/reducers/userNotification-reducer";
+import { addNotification, getUserNotificationsThunkCreator, removeUserNotificationsThunkCreator } from "../../redux/reducers/userNotification-reducer";
 import { getNotifications, getNotificationsCount } from "../../redux/selectors/userNotification-selectors";
 import { UserNotificationType } from "../../types/users/userNotificationType";
 import Notifications from "./Notifications";
-import * as signalR from "@microsoft/signalr";
 import { getInitialized } from "../../redux/selectors/app-selectors";
 import { getIsAuthenticated, getUserId } from "../../redux/selectors/user-selectors";
-import { SERVER_ADDRESS, setConnectionId } from "../../api/instanceAxios";
-import { hubsApiConstants } from "../../types/common/hubsApiConstants";
 import { notification } from "antd";
+import signalRUtil from "../../utils/signalRUtil";
 
 const NotificationsContainer: React.FC<PropsType> = (props) => {
     let notificationsBeingListened = false;
@@ -21,19 +19,7 @@ const NotificationsContainer: React.FC<PropsType> = (props) => {
             return;
         }
 
-        props.getNotifications();
-
-        notification.config({
-            placement: 'topRight',
-            duration: 3,
-            maxCount: 4
-          });
-
-        const connection = new signalR.HubConnectionBuilder()
-            .withUrl(`${SERVER_ADDRESS}/${hubsApiConstants.USER_NOTIFICATION_HUB_NAME}`)
-            .build();
-
-        connection.on(hubsApiConstants.GET_NOTIFICATION, data => {
+        signalRUtil.connectToNotificationHub(props.currentUserId as string, data => {
             data = JSON.parse(data);
             props.addNotification({ id: data.NotificationId, description: data.MessageNotification });
             notification.open({
@@ -41,10 +27,7 @@ const NotificationsContainer: React.FC<PropsType> = (props) => {
             });
         });
 
-        connection.start().then(() => {
-            setConnectionId(connection.connectionId as string);
-            connection.send(hubsApiConstants.SET_USER_INFO, connection.connectionId, props.currentUserId);
-        });
+        props.getNotifications();
 
         notificationsBeingListened = true;
     }, [props.appInitialized, props.isAuthenticated])
@@ -54,11 +37,13 @@ const NotificationsContainer: React.FC<PropsType> = (props) => {
 
 type MapDispatchToPropsType = {
     getNotifications: typeof getUserNotificationsThunkCreator,
+    removeNotification: typeof removeUserNotificationsThunkCreator,
     addNotification: typeof addNotification
 }
 
 const mapDispatchToProps = {
     getNotifications: getUserNotificationsThunkCreator,
+    removeNotification: removeUserNotificationsThunkCreator,
     addNotification: addNotification
 }
 
@@ -66,7 +51,7 @@ type MapStateToPropsType = {
     notifications: Array<UserNotificationType>,
     notificationCount: number,
     appInitialized: boolean,
-    currentUserId: string | undefined,
+    currentUserId?: string,
     isAuthenticated: boolean
 }
 
