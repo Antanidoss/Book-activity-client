30982e07df894538f2c5ae525ef10c0c6762ae71
Antanidsoss <antosci2002@gmail.com>
2024-06-09 19:04:12 +0300

    Added loadable route components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/redux-store'
@@ -30,5 +29,3 @@ root.render(
         </BrowserRouter>
     </StrictMode>,
 );
-
-reportWebVitals();
